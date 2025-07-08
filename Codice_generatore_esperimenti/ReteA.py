caa6f8e1db470a8b3742602ce90d528c9523260e
WrongMedal <graziosirosamaria@gmail.com>
2025-07-08 17:17:27 +0200

    Update ReteA.py
    Eliminazione di un commento risolto

--- Codice_generatore_esperimenti/ReteA.py
+++ Codice_generatore_esperimenti/ReteA.py
@@ -164,7 +164,6 @@ class MnistDataModule(pl.LightningDataModule):
 
 #@title Funzione utilitaria per esperimenti
 def run_experiment(seme, sample_size, layer, epochs, num_proj):
-    #Manca uso di sample_size nel data loader
     torch.manual_seed(seme)
     mnist_dm = MnistDataModule(seme, num_samples=sample_size)
     reteA = FNN_A((layer, layer, layer))
